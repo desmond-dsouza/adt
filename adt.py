bce1c11d62d290cedecb3f4299bdcff9d270c412
desmond <desmond@Desmond-Mac.local>
2014-12-16 15:18:02 -0600

    Refactor out tests into separate file, rename single adt.py

---
+++ adt.py
@@ -0,0 +1,100 @@
+from collections import namedtuple
+from typing import Union
+
+def Struct(*slots_with_types):
+    slots = tuple( 
+        [s_t[0] if isinstance(s_t, tuple) else s_t for s_t in slots_with_types])
+    tuplebase = namedtuple("_".join(slots) + "_Record", slots)
+    base_eq = getattr(tuplebase, '__eq__')
+    base_replace = getattr(tuplebase, '_replace')
+
+    def rep(self):
+        vals = ','.join(str(getattr(self, x)) for x in slots)
+        return "%s(%s)" % (type(self).__name__, vals)
+
+    def eq(self, other):
+        return type(self) == type(other) and base_eq(self, other)
+
+    setattr(tuplebase, '__repr__', rep)
+    setattr(tuplebase, '__eq__', eq)
+    setattr(tuplebase, 'whose', base_replace)
+
+    tuplebase.__annotations__ = slots_with_types
+    tuplebase.__is_struct__ = True
+    return tuplebase
+
+class Singleton:
+    @staticmethod
+    def __new__(cls):
+        if hasattr(cls, '__the__'):
+            return getattr(cls, '__the__')
+        else:
+            cls.__the__ = object.__new__(cls)
+            return cls.__the__
+
+    def __repr__(self):
+        return type(self).__name__ + "()"
+
+if __name__ == '__main__':
+
+    ### ##### Examples of Use (see tests for more details) #####
+
+    import typing as T
+
+    ### ##########################################
+    ### Simple structures, with or without methods
+    ### ##########################################
+    class City(Struct('name')):
+        pass
+
+    assert str(City('austin')) == 'City(austin)'
+
+    class Person(
+        Struct(
+            ('name', str),
+            'age',
+            ('cities', T.List[City]),
+            ('friends', T.List['Person']))):
+
+        def describe(self):
+            return self.name
+
+    p1 = Person('joe', 22, [], [])
+    p2 = p1.whose(name='steve')
+    assert p1 == Person('joe', 22, [], [])
+    assert p2 == Person('steve', 22, [], [])
+
+    ### ##########################################
+    ###  Union, including Singleton
+    ###  List = Empty() | Cons(hd, tl)
+    ### ##########################################
+
+    class Empty(Singleton):
+        def len(self):
+            return 0
+
+    E = Empty()
+
+    class Cons(Struct('hd', 'tl')):
+        def len(self):
+            return 1 + self.tl.len()
+
+    List = Union[Empty, Cons]
+
+    l1 = Cons(1, Cons(2, E))
+    assert l1.len() == 2
+
+    ### ##########################################
+    ### external function over a Union
+    ### map(f: T.Function, l: List) -> List
+
+    def map(f, l):
+        if isinstance(l, Cons):
+            return Cons(f(l.hd), map(f, l.tl))
+        elif l == E:
+            return Empty()
+        else: raise Exception()
+
+    assert map(lambda x: x+1, Cons(1, Cons(2, E))) == Cons(2, Cons(3, E))
+
+
---
+++ test_adts.py
@@ -0,0 +1,238 @@
+### ####################################################
+### Tests & Examples of Use
+
+from adt import Struct, Singleton, Union
+import typing as T
+
+### ################################################
+## Simple Struct Type
+class Person(
+    Struct(
+        ('name', str),
+        ('age', int),
+        ('cities', T.List[str]))):
+    def walk(self):
+        return "Walk"
+
+p = Person('bob', 22, ['austin', 'dallas'])
+
+def test_equality_update_repr():
+    assert p == Person('bob', 22, ['austin', 'dallas'])
+    assert p.whose(age=p.age + 1) == Person('bob', 23, ['austin', 'dallas'])
+    assert str(p) == "Person(bob,22,['austin', 'dallas'])"
+
+### ################################################
+## Nested Struct Type
+
+def test_nested_struct():
+
+    class Club(
+        Struct(
+            ('name', str),
+            ('members', T.List[Person]))):
+        def foo(self):
+            pass
+
+    s = Club('Soccer', [p, p.whose(name='Chris')])
+    assert str(s) == "Club(Soccer,[Person(bob,22,['austin', 'dallas']), Person(Chris,22,['austin', 'dallas'])])"
+
+### ################################################
+### Plain Union of 2 Structures
+### Event = Key(k, modifiers) | Mouse(position)
+### ################################################
+
+def test_plain_union_of_structs():
+    class Key(Struct('key', 'modifiers')):
+        pass
+
+    class Mouse(Struct('position')):
+        pass
+
+    Event = Union[Key, Mouse]
+
+### ################################################
+### Use normal Inheritance for Union with shared methods
+### Shape(...) = Rect(...) | Circle(...)
+### ################################################
+
+def test_union_with_methods():
+
+    class Shape:
+        def shared(self): return "I am a Shape"
+
+    class Rect(Shape,
+               Struct(
+                   ('x', int),
+                   ('y', int))):
+        def area(self): return self.x * self.y
+        def peri(self): return 2 * (self.x + self.y)
+
+    class Circle(Shape,
+                 Struct(
+                     ('r', int))):
+        def area(self): return 3.14 * self.r * self.r
+        def peri(self): return 2 * 3.14 * self.r
+
+    ShapeUnion = Union[Rect, Circle]
+
+    r = Rect(10, 20)
+    c = Circle(20)
+
+    assert isinstance(r, Shape)
+    assert isinstance(r, Rect)
+    assert not isinstance(r, Circle)
+    assert isinstance(c, Shape)
+    assert r.shared() == "I am a Shape"
+    assert r.area() == 200
+    assert str(Rect(4,5))  == "Rect(4,5)"
+    assert Rect.__annotations__ == (('x', int), ('y', int))
+
+### ################################################
+## Just for comparison: Python 3.4 has built-in Enum
+## to enumerate values
+from enum import Enum
+class IntColor(Enum):
+    Red = 1
+    Blue = 2
+    Green = 3
+
+## Enum values can be structured objects, isinstance will work for those values
+## - freely created values will NOT pass isinstance (even if equal to enum value)
+class RGB(Struct('r','g', 'b')):
+    pass
+
+class Color(Enum):
+    Red = RGB(1,0,0)
+    Blue = RGB(0,1,0)
+    Green = RGB(0,0,1)
+
+def test_enums():
+    assert isinstance(Color.Red, Color)
+    assert not isinstance(RGB(1,0,0), Color)
+
+### ################################################
+## Unions can contain @singletons defined as classes with methods (but no Record fields)
+## The singleton gets a custom __repr__
+
+### ################################################
+## List = Empty | Cons(h, tl)
+### ################################################
+
+class Cons(Struct('hd', 'tl')):
+    def len(self):
+        return 1 + self.tl.len()
+
+class Empty(Singleton):
+    def len(self):
+        return 0
+
+List = Union[Cons,Empty]
+
+E = Empty()
+
+def test_list_cons_empty():
+
+    assert Empty() is Empty()
+
+    l2 = Cons(2, Cons(3, E))
+    assert str(E) == "Empty()"
+    assert E.len() == 0
+    assert l2.len() == 2
+
+## A pattern-matching MAP over List
+def test_pattern_match_over_list():
+    def map(f: T.Function, l: List) -> List:
+        if isinstance(l, Empty):
+            return E
+        elif isinstance(l, Cons):
+            return Cons(f(l.hd), map(f, l.tl))
+        else: raise Exception("Not in List Union")
+
+    l2 = Cons(2, Cons(3, E))
+    assert map(lambda x: x + 1, l2) == Cons(3, Cons(4, E))
+
+### Peano Arithmetic example
+### ################################################
+### Num = Zero | S(n)
+
+class S(Struct('n')):
+    def __add__(self, x):
+        return self.n + S(x)
+
+class Zero(Singleton):
+    def __add__(self, x):
+        return x
+
+def test_peano():
+    Z = Zero()
+    _1 = S(Z)
+    _2 = S(_1)
+    _3 = S(_2)
+    assert str(Z) == "Zero()"
+    assert _3 == S(S(S(Z)))
+    assert Z + S(Z) == S(Z)
+    assert _1 + _2 == _3
+    assert _2 + _1 == _3
+
+### ################################################
+### Elm Collage -- selected bits
+
+## Elm does extensible record typing : Python does duck typing
+class Form(Struct(
+    'theta',
+    'scale',
+    'x',
+    'y',
+    'alpha',
+    'form')):
+    pass
+
+######
+class Solid(Struct('color')): pass
+class Texture(Struct('url')): pass
+class Grad(Struct('gradient')): pass
+FillStyle = Union[Solid, Texture, Grad]
+
+
+class Flat(Singleton): pass
+class Round(Singleton): pass
+class Padded(Singleton): pass
+LineCap = Union[Solid, Texture, Grad]
+
+
+class FPath(Struct('linestyle', 'path')): pass
+class FShape(Struct('shapestyle', 'shape')): pass
+class FImage(Struct('w', 'h', 'pos', 'url')): pass
+class FGroup(Struct('transform2D', 'forms')): pass
+BasicForm = Union[FPath, FShape, FImage, FGroup]
+
+
+class Line(Struct('linestyle')): pass
+class Fill(Struct('fillstyle')): pass
+ShapeStyle = Union[Line, Fill]
+
+
+class Shape(Struct('point_list')): pass
+
+def test_elm_graphics():
+
+    def form(f: BasicForm) -> Form:
+        return Form(theta=0,scale=1,x=0,y=0,alpha=1,form=f)
+
+    assert form("myBasicForm") == Form(0,1,0,0,1,"myBasicForm")
+
+    def _fill(fillstyle, shape):
+        return form(FShape(Fill(fillstyle), shape))
+
+    def filled(c: Color, sh: Shape) -> Form:
+        return _fill(Solid(c), sh)
+
+    s = Shape([(0,0), (1,1), (2,2)])
+    f = filled(Color.Red, s)
+    assert f == Form(0,1,0,0,1,FShape(Fill(Solid(Color.Red)),s))
+
+    def movex(dx: int, f: Form) -> Form:
+        return f.whose(x=f.x + dx)
+
+    f2 = movex(20, f)
+    assert f2 == Form(0,1,20,0,1,FShape(Fill(Solid(Color.Red)),s))
